11631cf0d7f25fd00e8ebf6e4f323488cf4695c8
Terrance <terrance.hanlon@ndsu.edu>
2019-07-30 19:21:10 -0500

    flask

---
+++ API/flask/server.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import request
+from flaskext.mysql import MySQL
+from flask import g
+# from flask.config import Config
+import config
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+mysql = MySQL()
+
+
+app.config['MYSQL_DATABASE_HOST'] = config.host
+app.config['MYSQL_DATABASE_USER'] = config.user
+app.config['MYSQL_DATABASE_PASSWORD'] = config.password
+app.config['MYSQL_DATABASE_DB'] = config.db
+
+mysql.init_app(app)
+
+@app.route("/")
+def hello():
+	print(type(os.getenv('host')))
+	print(os.getenv('password'))
+	return "Hello world"
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+	if request.method == 'POST':
+		print(type(os.getenv('host')))
+		# print(request.get_json()['accName'])
+		db = mysql.connect()
+		cur = db.cursor()
+		print(request.get_json())
+		name = request.get_json()['name']
+		cur.execute("INSERT INTO user(name) VALUES (%s)", name)
+		db.commit()
+		cur.close()
+		return "done"
+
+
+
+if __name__ == "__main__":
+	app.run()
